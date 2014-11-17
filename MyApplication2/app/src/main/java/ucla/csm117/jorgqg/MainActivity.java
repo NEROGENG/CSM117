cd2e05092c657f4b498f633ac09c907c31460d6c
NEROGENG <szmun.gengqj@gmail.com>
2014-11-16 16:48:54 -0800

    Retrieve the current location

--- MyApplication2/app/src/main/java/ucla/csm117/jorgqg/MainActivity.java
+++ MyApplication2/app/src/main/java/ucla/csm117/jorgqg/MainActivity.java
@@ -1,31 +1,67 @@
 package ucla.csm117.jorgqg;
 
 import android.app.Activity;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
-
-/**import android.app.Activity;
-import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Init!");
+        setUpIfNeeded();
     }
 
+    private void setUpIfNeeded() {
+        if (googleMap == null) {
+            googleMap = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            Log.d(TAG, "Acquire Map!");
+            if (googleMap != null) {
+                Log.d(TAG, "Got Map!");
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        googleMap.setMyLocationEnabled(true);
+
+        LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        double latitude = myLocation.getLatitude();
+        double longitude = myLocation.getLongitude();
+
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(20));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,19 +69,4 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}*/
+}
