26db73f89d6ac5b089585e8acec735e9e2ca5db4
NEROGENG <szmun.gengqj@gmail.com>
2014-11-23 17:41:05 -0800

    update markers function implemented

--- MyApplication2/app/src/main/java/ucla/csm117/jorgqg/MainActivity.java
+++ MyApplication2/app/src/main/java/ucla/csm117/jorgqg/MainActivity.java
@@ -3,38 +3,39 @@ package ucla.csm117.jorgqg;
 import android.app.Activity;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
-import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends FragmentActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-
     private GoogleMap googleMap;
+    private LocationManager locationManager;
+    private List<Marker> markerList = new ArrayList<Marker>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "Init!");
         setUpIfNeeded();
     }
 
     private void setUpIfNeeded() {
         if (googleMap == null) {
             googleMap = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-            Log.d(TAG, "Acquire Map!");
             if (googleMap != null) {
-                Log.d(TAG, "Got Map!");
                 setUpMap();
             }
         }
@@ -42,25 +43,44 @@ public class MainActivity extends FragmentActivity {
 
     private void setUpMap() {
         googleMap.setMyLocationEnabled(true);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
-        LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
 
         Criteria criteria = new Criteria();
 
         String provider = locationManager.getBestProvider(criteria, true);
 
-        Location myLocation = locationManager.getLastKnownLocation(provider);
+        Location location = locationManager.getLastKnownLocation(provider);
 
-        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //googleMap.clear();
+                drawMarker(location);
+            }
 
-        double latitude = myLocation.getLatitude();
-        double longitude = myLocation.getLongitude();
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
 
-        LatLng latLng = new LatLng(latitude, longitude);
+            }
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(20));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+            if(location != null){
+                //PLACE THE INITIAL MARKER
+                drawMarker(location);
+            }
+            locationManager.requestLocationUpdates(provider, 10000, 20, locationListener);
     }
 
     @Override
@@ -69,4 +89,22 @@ public class MainActivity extends FragmentActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    private void drawMarker(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        googleMap.addMarker(new MarkerOptions().position(latLng));
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        //googleMap.animateCamera(CameraUpdateFactory.zoomTo(20));
+    }
+
+    public void updateMarkers(List<Location> locationList) {
+        googleMap.clear();
+        int iterator;
+        for (iterator = 0; iterator < locationList.size(); iterator++)
+            drawMarker(locationList.get(iterator));
+    };
 }
